32c1100f230dcc1c8e89189a60489bddbbc8f498
Fabien-B <fabien.bonneval@gmail.com>
2018-07-21 01:16:24 +0200

    Adds terminal history.

--- atconfigurator.py
+++ atconfigurator.py
@@ -9,12 +9,14 @@ from serial_monitor import SerialMonitor
 
 class ATConfigurator(Ui_MainWindow):
   
-
   def __init__(self, filename, parent=None):
     Ui_MainWindow.__init__(self)
     self.commands = []
     self.data = self.parse(filename)
     self.serial_monitor = None  
+    self.filter = None
+    self.serial_history = [""]    # contains the history of the hand typed commands
+    self.index_history = 0
   
   def built(self):
     self.serial_monitor = SerialMonitor("", 42, self.serial_textEdit)
@@ -24,6 +26,8 @@ class ATConfigurator(Ui_MainWindow):
     self.atw_button.clicked.connect(self.at_write)
     self.serial_lineEdit.returnPressed.connect(self.write_line_to_serial)
     self.crlf_comboBox.currentIndexChanged.connect(self.serial_monitor.change_ending)
+    self.filter = Filter(self.serial_lineEdit, self.command_history_up, self.command_history_down)
+    self.serial_lineEdit.installEventFilter(self.filter)
   
   def add_AT_ui(self):
     for command in self.commands:
@@ -38,10 +42,22 @@ class ATConfigurator(Ui_MainWindow):
   def at_write(self):
     print("write")
     self.serial_monitor.write("AT&W\r\n")
+  
   def write_line_to_serial(self):
     text = self.serial_lineEdit.text()
     self.serial_lineEdit.clear()
+    if text:
+      self.serial_history.append(text)
+    self.index_history = 0
     self.serial_monitor.write_ending(text)
+  
+  def command_history_up(self):
+    self.index_history = max(self.index_history - 1, -(len(self.serial_history)-1))
+    self.serial_lineEdit.setText(self.serial_history[self.index_history])
+    
+  def command_history_down(self):
+    self.index_history = min(self.index_history + 1, 0)
+    self.serial_lineEdit.setText(self.serial_history[self.index_history])
     
   def parse(self, filename):
     with open(filename, 'r') as f:
@@ -58,3 +74,19 @@ class ATConfigurator(Ui_MainWindow):
           command.params.append(param)
         self.commands.append(command)
       return data
+
+class Filter(QtCore.QObject):
+  def __init__(self, widget, up_callback, down_callback):
+      super(QtCore.QObject, self).__init__()
+      self.widget_concerned = widget
+      self.up_callback = up_callback
+      self.down_callback = down_callback
+
+  def eventFilter(self, source, event):
+    if (event.type() == QtCore.QEvent.KeyPress and source is self.widget_concerned):
+      if event.key() == QtCore.Qt.Key_Up:
+        self.up_callback()
+      elif event.key() == QtCore.Qt.Key_Down:
+        self.down_callback()
+      
+    return False
