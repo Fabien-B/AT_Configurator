92e5f060e45652d0613c6391d77e1a1a59496bda
Fabien-B <fabien.bonneval@gmail.com>
2018-07-23 14:06:11 +0200

    Take config file as argument.

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
 import atconfigurator
+import argparse
 
-def main():
+def main(config_file):
   app = QtWidgets.QApplication(sys.argv)
   MainWindow = QtWidgets.QMainWindow()
-  configurator = atconfigurator.ATConfigurator('hm-trlr-s.json')
+  configurator = atconfigurator.ATConfigurator(config_file)
   app.aboutToQuit.connect(configurator.closing)
   configurator.setupUi(MainWindow)
   configurator.built()
@@ -15,4 +16,7 @@ def main():
 
 
 if __name__ == '__main__':
-  main()
+  parser = argparse.ArgumentParser(description="Interface for AT device configuration")
+  parser.add_argument('config_file', help="JSON configuration file")
+  args = parser.parse_args()
+  main(args.config_file)
