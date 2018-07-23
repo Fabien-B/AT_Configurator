f20d1b29ba3ba416762ff21e7bc0c522df49d6c3
Fabien-B <fabien.bonneval@gmail.com>
2018-07-23 02:22:29 +0200

    Adds serial read/write methods.

--- serial_monitor.py
+++ serial_monitor.py
@@ -1,10 +1,17 @@
 import serial
+from PyQt5 import QtCore, QtGui
 
 class SerialMonitor():
   def __init__(self, serial_textEdit):
     self.ser = None
+    self.serial_mutex = QtCore.QMutex()
     self.ending = ""
     self.serial_textEdit = serial_textEdit
+    self.serial_timer = QtCore.QTimer()
+    self.serial_timer.setInterval(200)
+    self.serial_timer.timeout.connect(self.read_serial)
+    self.read_buffer = ""
+  
   def connect(self, port, baud):
     self.ser = serial.Serial(port, baud)
     self.serial_timer.start()
@@ -15,10 +22,28 @@ class SerialMonitor():
     self.ser = None
   
   def write(self, string):
-    self.serial_textEdit.append(">>>" + string)  #TODO faire en bien
+    if self.ser is not None:
+        self.serial_mutex.lock()
+        self.serial_textEdit.insertPlainText("\n>>>" + string + "\n")  #TODO faire en bien
+        self.serial_textEdit.moveCursor(QtGui.QTextCursor.End)
+        self.ser.write(string.encode())
+        self.serial_mutex.unlock()
   
   def write_ending(self, string):
     self.write(string+self.ending)
+  
+  def read_serial(self):
+    if self.ser is not None:
+      self.serial_mutex.lock()
+      if self.ser.in_waiting:
+        data = self.ser.read(self.ser.in_waiting)
+        string = data.decode()
+        
+        
+        
+        self.serial_textEdit.insertPlainText(string)
+        self.serial_textEdit.moveCursor(QtGui.QTextCursor.End)
+      self.serial_mutex.unlock()
   
   def change_ending(self, index):
     if index == 0:
